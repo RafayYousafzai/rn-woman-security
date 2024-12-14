c7d6c6428c1389d5eb3cb1e382428c39f7b35679
rafay <rafaykhan.mesh@gmail.com>
2024-12-14 16:13:57 +0500

    Feat: UI

--- app/(root)/(tabs)/emergency-alert.jsx
+++ app/(root)/(tabs)/emergency-alert.jsx
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import * as React from 'react';
+import { View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
+import TrustedContacts from '../../../components/TrustedContacts';
 
-const EmergencyAlert = () => {
+export default function EmergencyAlertPage() {
   return (
-    <View>
-      <Text>EmergencyAlert</Text>
-    </View>
-  )
-}
+    <View className="flex-1 bg-gray-100 p-4 justify-center items-center">
+      <Text className="text-2xl font-bold text-gray-800 mb-6">Emergency Alert</Text>
 
-export default EmergencyAlert
+      <Button
+        mode="contained"
+        className="w-full bg-red-500 py-2 mb-6"
+        onPress={() => console.log('Alert Sent to Trusted Contacts!')}
+      >
+        Send Alert
+      </Button>
+
+      <Text className="text-lg font-medium text-gray-700 mb-4">Settings</Text>
+
+     <TrustedContacts/>
+    </View>
+  );
+}
